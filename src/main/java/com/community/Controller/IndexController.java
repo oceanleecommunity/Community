f5b0411f0c83f29ac4134a30a5d2bcc40df8c726
792116916 <792116916@qq.com>
2019-08-18 22:54:11 +0800

    Ocean -- first commit

--- src/main/java/com/community/Controller/IndexController.java
+++ src/main/java/com/community/Controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.community.Controller;
 
+import com.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/hello")
+
+
+    @GetMapping("hello")
     public String hello() {
         return "index";
     }
---
+++ src/main/java/com/community/dto/GithubUser.java
@@ -0,0 +1,33 @@
+package com.community.dto;
+
+import java.math.BigInteger;
+
+public class GithubUser {
+    private String name;
+    private Integer id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
